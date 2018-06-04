08ee5d6800071b1c2001f23efc5b547034e599e8
dpcosta <dpcosta@gmail.com>
2018-06-04 16:17:42 -0300

    Criação de um controlador responsável pela API de listas de leitura. Ainda utilizando roteamento MVC (controller/action).

---
+++ Alura.WebAPI.WebApp/Controllers/ListaLeituraController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.WebAPI.Model;
+using Alura.WebAPI.WebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.WebAPI.WebApp.Controllers
+{
+    public class ListaLeituraController : Controller
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListaLeituraController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        private IEnumerable<Livro> LivrosDoTipo(TipoListaLeitura tipo)
+        {
+            return _repo
+                .All
+                .Where(l => l.Lista == tipo)
+                .ToList();
+        }
+
+        public IActionResult ParaLer()
+        {
+            var lista = LivrosDoTipo(TipoListaLeitura.ParaLer);
+            return Ok(lista);
+        }
+
+        public IActionResult Lidos()
+        {
+            var lista = LivrosDoTipo(TipoListaLeitura.Lidos);
+            return Ok(lista);
+        }
+
+        public IActionResult Lendo()
+        {
+            var lista = LivrosDoTipo(TipoListaLeitura.Lendo);
+            return Ok(lista);
+        }
+    }
+}
