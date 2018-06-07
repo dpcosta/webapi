9eb0359ed9aa7ff089d9808422f83e6f7a8be352
dpcosta <dpcosta@gmail.com>
2018-06-07 11:37:11 -0300

    Concluindo o CRUD na API de livros (verbos POST, PUT e DELETE)

--- Alura.WebAPI.API/Controllers/LivrosController.cs
+++ Alura.WebAPI.API/Controllers/LivrosController.cs
@@ -37,5 +37,42 @@ namespace Alura.WebAPI.WebApp.API
             }
             return Ok(livro.ToApi());
         }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] LivroCapaAsMultipartData model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Get", new { Id = livro.Id });
+                return Created(uri, livro);
+            }
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] LivroCapaAsMultipartData model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Alterar(livro);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var livro = _repo.Find(id);
+            if (livro != null)
+            {
+                _repo.Excluir(livro);
+                return NoContent();
+            }
+            return NotFound();
+        }
     }
 }
--- Alura.WebAPI.API/Models/LivroApi.cs
+++ Alura.WebAPI.API/Models/LivroApi.cs
@@ -1,10 +1,41 @@
 ﻿using Alura.WebAPI.Model;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Xml.Serialization;
 
 namespace Alura.WebAPI.API.Models
 {
     public static class LivroApiExtensions
     {
+        public static byte[] ConvertToBytes(IFormFile image)
+        {
+            if (image == null)
+            {
+                return null;
+            }
+            using (var inputStream = image.OpenReadStream())
+            using (var stream = new MemoryStream())
+            {
+                inputStream.CopyTo(stream);
+                return stream.ToArray();
+            }
+        }
+
+        public static Livro ToLivro(this LivroCapaAsMultipartData model)
+        {
+            return new Livro
+            {
+                Id = model.Id,
+                Titulo = model.Titulo,
+                Subtitulo = model.Subtitulo,
+                Resumo = model.Resumo,
+                Autor = model.Autor,
+                ImagemCapa = ConvertToBytes(model.Capa),
+                Lista = model.Lista
+            };
+        }
+
         public static LivroApi ToApi(this Livro livro)
         {
             return new LivroApi
@@ -30,5 +61,17 @@ namespace Alura.WebAPI.API.Models
         public string Resumo { get; set; }
         public string Capa { get; set; }
         public string Lista { get; set; }
+    }
+
+    public class LivroCapaAsMultipartData
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public string Autor { get; set; }
+        public string Resumo { get; set; }
+        public IFormFile Capa { get; set; }
+        public TipoListaLeitura Lista { get; set; }
     }
 }
